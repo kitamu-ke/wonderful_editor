f748f87e1ee98e5f4fe850403763048b83de7932
KitamuraKen <k.ken0324.soccer2610@gmail.com>
2023-08-05 15:01:33 +0900

    テストの部分を修正した。

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Articles" do
   describe "GET /articles" do
     subject { get(api_v1_articles_path) }
 
-    before(:article) { create_list(:article, 3) }
+    before {3.times{create(:article)}}
     # before {3.times{create(:article)}}
     # let!(:article1) { create(:article, updated_at: 1.days.ago) }
     # let!(:article2) { create(:article, updated_at: 2.days.ago) }
@@ -12,8 +12,9 @@ RSpec.describe "Articles" do
 
     it "記事の一覧が取得できる" do
       subject
-      res = response.parsed_body.
-              expect(response).to have_http_status(:ok)
+      res = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:ok)
       expect(res.length).to eq 3
       # expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
       # expect(res[0].keys).to eq ["id", "body", "title", "updated_at", "user"]
