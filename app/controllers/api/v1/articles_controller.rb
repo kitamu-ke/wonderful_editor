c8e7745aae2275af1ecc51145811a1046598728d
KitamuraKen <k.ken0324.soccer2610@gmail.com>
2023-08-05 17:27:46 +0900

    showを実装した。

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,7 +2,12 @@ module Api::V1
   class ArticlesController < BaseApiController
     def index
       articles = Article.order(updated_at: :desc)
-      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+      render json: articles, serializer: Api::V1::ArticlePreviewSerializer
+    end
+
+    def show
+      article = Article.find(params[:id])
+      render json: article, serializer: Api::V1::ArticlePreviewSerializer
     end
   end
 end
